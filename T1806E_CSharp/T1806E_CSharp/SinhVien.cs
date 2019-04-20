6f2c19748823ad1ec82be8d7bfee78e3d6c91c60
kylix.par <kylix.par@hotmail.com>
2019-04-20 18:32:12 +0700

    test

---
+++ T1806E_CSharp/T1806E_CSharp/SinhVien.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T1806E_CSharp
+{
+    class SinhVien
+    {
+        public string name;
+        public int age;
+        public int mark;
+    }
+    public void NhapThongTin()
+    {
+
+    }
+    public void InThongTin()
+    {
+
+    }
+    public void BaoKetQua()
+    {
+
+    }
+}
