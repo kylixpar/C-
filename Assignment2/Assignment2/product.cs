d4ff01549500f01a91069a4efc6c7fc1660e8f3f
kylix.par <kylix.par@hotmail.com>
2019-04-23 13:16:31 +0700

    Assignment2

---
+++ Assignment2/Assignment2/product.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Assignmen2
+{
+    public class Product
+    {
+        string name;
+        int id;
+        float price;
+        int qty;
+        string image;
+        string desc;
+        List<string> gallery = new List<string>();
+
+
+        public Product()
+        {
+
+        }
+        public float GetPrice()
+        {
+            return price;
+        }
+        public Product(int id, string name, float price, int qty, string image, string desc, List<string> gallery)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.qty = qty;
+            this.image = image;
+            this.desc = desc;
+            for (int i = 0; i < gallery.Count && i < 10; i++)
+            {
+                gallery.Add(gallery[i]);
+            }
+
+        }
+        public void getInfo()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Price: " + price);
+            Console.WriteLine("Qty: " + qty);
+            Console.WriteLine("Image: " + image);
+            Console.WriteLine("Desc: " + desc);
+        }
+        public int GetQty()
+        {
+            return qty;
+        }
+        public void AddGallery(string img)
+        {
+            if (gallery.Count < 10)
+            {
+                gallery.Add(img);
+            }
+            else
+            {
+                Console.WriteLine("Gallery is full. ");
+            }
+        }
+        public void RemoveGallery()
+        {
+            Console.WriteLine("Remove at? ");
+            int i = Int16.Parse(Console.ReadLine());
+            gallery.RemoveAt(i);
+        }
+
+    }
+}
